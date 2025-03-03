622410fb5c9d43bea03ef088506617904a3befae
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-03 18:00:08 -0300

    adding commentss

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,7 +1,7 @@
 gcc dependencies/darwin.c -o darwin.out
-#se building trusted anchors
 cd dependencies/BearSSL
 make
 cd ../..
 chmod +x dependencies/BearSSL/build/brssl
+#call the brssl tool to generate the trust anchors 
 ./dependencies/BearSSL/build/brssl ta dependencies/cacert.pem > dependencies/BearSSLTrustAnchors.h
